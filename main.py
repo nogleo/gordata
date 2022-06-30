b7dc50b47ea47de21f6d66f7ee278d6a3ba769ab
nogleo <leo.nog88@gmail.com>
2022-06-30 17:32:56 -0300

    fix pull on calibration

--- main.py
+++ main.py
@@ -250,7 +250,7 @@ class app_gd(qtw.QMainWindow):
                 logging.debug('collecting position  ' + str(ii+1))
                 try:
                     self.calibrationdata[ii*self.NS:(ii+1)*self.NS,:] = dq.pull_data(durr=TS,
-                                                                                 devices=device)['Sensor_{}'.format(hex(addr))].to_numpy()
+                                                                                 devices=device)[['Sensor_{}'.format(hex(addr))]].to_numpy()
                 except Exception as e:
                     logging.warning('can`t pull data', exc_info=e)
             else:
@@ -266,7 +266,7 @@ class app_gd(qtw.QMainWindow):
             if ok:
                 logging.info('collecting rotation  ' + str(ii+1))
                 self.calibrationdata[6*self.NS+ii*self.ND:6*self.NS+(ii+1)*self.ND] = dq.pull_data(durr=TD, 
-                                                                                                    devices=device).to_numpy()                
+                                                                                                    devices=device)[['Sensor_{}'.format(hex(addr))]].to_numpy()                
             else:
                 logging.debug('cancelled')
                 return
